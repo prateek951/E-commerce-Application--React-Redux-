0de581db11c55dd83d2ed8c8210d3bd66df88178
prateek951 <prince.cenation@gmail.com>
2019-07-16 14:18:39 +0530

    setting up the shop dummy data and the shop page

---
+++ shopping-store/src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,26 @@
+/**
+ * @component Preview Collection Component
+ * which will house all of the collection items pertaining to
+ * a specific collection
+ *
+ *
+ */
+
+import React from 'react';
+import './preview-collection.styles.scss';
+const PreviewCollection = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, index) => index < 4)
+          .map(item => (
+            <div key={item.id}>{item.name}</div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default PreviewCollection;
