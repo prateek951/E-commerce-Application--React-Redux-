340220129e4ebbe2f134a22f0ee359f4bf7a66f7
prateek951 <prince.cenation@gmail.com>
2019-07-16 14:27:35 +0530

    collection preview component,collection item pertaining to them, structure laid

--- shopping-store/src/components/preview-collection/preview-collection.component.jsx
+++ shopping-store/src/components/preview-collection/preview-collection.component.jsx
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import './preview-collection.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 const PreviewCollection = ({ title, items }) => {
   return (
     <div className="collection-preview">
@@ -15,8 +16,8 @@ const PreviewCollection = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
-          .map(item => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))}
       </div>
     </div>
