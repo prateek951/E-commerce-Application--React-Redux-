71aaf9fd67e8826c8f0fd0810edaf413ebfcf777
prateek951 <prince.cenation@gmail.com>
2019-07-19 11:59:13 +0530

    cart count is now being shown

--- shopping-store/src/components/cart-icon/cart-icon.component.jsx
+++ shopping-store/src/components/cart-icon/cart-icon.component.jsx
@@ -3,11 +3,11 @@ import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleDropdown } from '../../redux/cart-item/cart-item.actions';
-const CartIcon = ({ toggleDropdown }) => {
+const CartIcon = ({ toggleDropdown, itemsCount }) => {
   return (
     <div className="cart-icon" onClick={() => toggleDropdown()}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemsCount}</span>
     </div>
   );
 };
@@ -19,7 +19,14 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   };
 };
 
+const mapStateToProps = state => ({
+  itemsCount: state.cart.cartItems.reduce((netQuantity, cartItem) => {
+    netQuantity += cartItem.quantity;
+    return netQuantity;
+  }, 0)
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
