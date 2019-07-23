f6cadbae517e9f5acfa77c0ac4827691f205078b
prateek951 <prince.cenation@gmail.com>
2019-07-23 08:53:13 +0530

    redux logger only in development mode

--- shopping-store/src/redux/store.js
+++ shopping-store/src/redux/store.js
@@ -4,7 +4,13 @@ import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
 // 1. Create middlewares
-const middlewares = [logger];
+const middlewares = [];
+
+// 1.1 Check for the environment if it is development mode only then make use of logger 
+if(process.env.NODE_ENV === 'development') { 
+  middlewares.push(logger);
+}
+
 
 // 2. Create store
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
